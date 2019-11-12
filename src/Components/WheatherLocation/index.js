88b3fdf16e99853988ef79598fc7bef68a9d3e5f
CodeVicium <46329897+CodeVicium@users.noreply.github.com>
2019-11-12 11:54:49 -0300

    Arregle el problema de la api y converti las metricas

--- src/Components/WheatherLocation/index.js
+++ src/Components/WheatherLocation/index.js
@@ -1,10 +1,11 @@
 import React, {Component} from 'react';
 import Location from './Location';
 import WheatherData from './WeatherData';
+import convert from 'convert-units'
 import {SNOW,WINDY,SUN,RAIN,CLOUD,CLOUDY,} from '../../Constants/wheaters';
 import './style.css';
 
-const location = "Buenos Aires,ar";
+const location = "Cordoba ,ar";
 const api_key = "ac17291dab98d4865910f10a80f2b8d1";
 const url_base_wheather = "http://api.openweathermap.org/data/2.5/forecast";
 
@@ -40,20 +41,24 @@ class WheatherLocation extends Component{
             data:data,
         };
     }
-
+    getTemp=Kelvin=>{
+     return  Number(convert(Kelvin).from("K").to("C").toFixed(2)) ;
+    }
     getWheatherState = wheather_data=>{
         return SUN;
     }
 
 
     getData = wheather_data => {
-        const {humidity,temp} = wheather_data.main;
-        const {speed}=wheather_data.wind;
-        const wheatherState = this.getWheatherState(wheather_data);
+
+        const { humidity,temp } = wheather_data.list[0].main;
+        const {speed} = wheather_data.list[0].wind;
+        const wheatherState =  this.getWheatherState(wheather_data);
+        const temperature = this.getTemp(temp);
 
         const data={
             humidity,
-            temperature: temp,
+            temperature,
             wheatherState,
             wind:`${speed} m/s`,
         }
@@ -68,14 +73,10 @@ class WheatherLocation extends Component{
             return resolve.json(); 
         }).then(data =>{
 
-            // const newWeather = this.getData(data);
-            // console.log(newWeather);
-            // debugger;
-            // this.setState({
-            //     data:newWeather
-            // });
-             console.log(data);
-             debugger;
+             const newWeather = this.getData(data);
+            //  console.log(newWeather);
+            //  debugger;
+             this.setState({data:newWeather});
         });
 
         // console.log("Actualizado");
